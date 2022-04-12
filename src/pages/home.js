3b56d06e0e5d749381c7d92db187c8c54ef82695
pivattoo <pivattogui@gmail.com>
2022-04-12 20:35:29 -0300

    feat: create project

---
+++ src/pages/home.js
@@ -0,0 +1,20 @@
+import '../App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>
+                    Sistema de previsão do tempo
+                </h1>
+                <p>Utilizar</p>
+                <a href="http://servicos.cptec.inpe.br/XML/">Api para previsão do tempo</a>
+                <a href="https://axios-http.com/ptbr/docs/intro">Axios para requisições HTTP</a>
+                <a href="https://mui.com/pt/">MUI para ficar mais bunitinho</a>
+
+            </header>
+        </div>
+    );
+}
+
+export default App;
